c0d1dba5e4ed54d8899be5e2c6b11d1082032339
Aditya Brahmankar <adityabrahmankar9@gmail.com>
2023-07-21 14:57:06 +0530

    initial commit

---
+++ app/(routes)/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const About = () => {
+    const router = useRouter();
+
+    return ( 
+        <h1 className="text-white">About</h1>
+     );
+}
+ 
+export default About;
