c0676eac4c75e742a8f86dd50990912de08eea54
Aditya Brahmankar <adityabrahmankar9@gmail.com>
2023-12-06 21:12:14 +0530

    style : Navbar motion

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,13 @@
 "use client";
-
-import { usePathname } from "next/navigation";
+import { m } from "framer-motion";
 import Link from "next/link";
 
 import { MenuItems } from "@/lib/constants";
 import useMediaQuery from "@/lib/use-mediaquery";
+import { useState } from "react";
 
 const Navbar = () => {
-  const pathname = usePathname();
+  const [activeTab, setActiveTab] = useState(MenuItems[0].href);
 
   const { isMobile } = useMediaQuery();
 
@@ -15,12 +15,26 @@ const Navbar = () => {
     return (
       <div className="fixed bottom-0 left-0 right-0 z-20 flex flex-row items-center justify-around px-3 py-4 text-white bg-gray-500/10 backdrop-blur-sm ">
         {MenuItems.map((item) => (
-          <Link href={item.href} key={item.title}>
-            <item.icon
-              className={`w-8 h-8 ${
-                pathname === item.href ? "text-muted" : "text-muted-foreground"
-              }`}
-            />
+          <Link
+            href={item.href}
+            key={item.title}
+            onClick={() => setActiveTab(item.href)}
+            className={`${
+              activeTab === item.href ? "" : "hover:text-white/60"
+            } relative rounded-full px-3 py-1.5 text-sm font-medium text-white outline-sky-400 transition focus-visible:outline-2`}
+            style={{
+              WebkitTapHighlightColor: "transparent",
+            }}
+          >
+            {activeTab === item.href && (
+              <m.span
+                layoutId="bubble"
+                className="absolute inset-0 z-10 bg-white mix-blend-difference"
+                style={{ borderRadius: 9999 }}
+                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+              />
+            )}
+            <item.icon />
           </Link>
         ))}
       </div>
@@ -32,10 +46,22 @@ const Navbar = () => {
         <Link
           href={item.href}
           key={item.title}
-          className={`px-3 py-2 hover:bg-slate-500/10 rounded-full ${
-            pathname === item.href ? "underline underline-offset-4" : ""
-          }`}
+          onClick={() => setActiveTab(item.href)}
+          className={`${
+            activeTab === item.href ? "" : "hover:text-white/60"
+          } relative rounded-full px-3 py-1.5 text-sm font-medium text-white outline-sky-400 transition focus-visible:outline-2`}
+          style={{
+            WebkitTapHighlightColor: "transparent",
+          }}
         >
+          {activeTab === item.href && (
+            <m.span
+              layoutId="bubble"
+              className="absolute inset-0 z-10 bg-white mix-blend-difference"
+              style={{ borderRadius: 9999 }}
+              transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+            />
+          )}
           {item.title}
         </Link>
       ))}
